ef0e8e4156028bf009d9ebc7e39647c478ba6588
VelinYordanov <velin.iordanov@gmail.com>
2018-11-01 07:56:14 +0200

    A few fixes

--- DotaHelper.Web/wwwroot/js/items.js
+++ DotaHelper.Web/wwwroot/js/items.js
@@ -1,25 +1,27 @@
-﻿function showItems() {
-    const itemsImages = $('.dota-items');
-    const images = $('.dota-item-image');
+﻿(function () {
+    function showItems() {
+        const itemsImages = $('.dota-items');
+        const images = $('.dota-item-image');
 
-    if (!(itemsImages && images)) {
-        return;
+        if (!(itemsImages && images)) {
+            return;
+        }
+
+        images.hover(event => {
+            const target = $(event.target);
+            const itemDiv = target.next('div');
+            const x = event.clientX;
+            const y = event.clientY;
+            const itemDivWidth = itemDiv.outerWidth();
+            const itemDivHalfHeight = itemDiv.height() / 2;
+            itemDiv.css({ top: (y - itemDivHalfHeight) + 'px', left: (x - itemDivWidth) + 'px' });
+            itemDiv.show();
+        }, event => {
+            const target = $(event.target);
+            const itemDiv = target.next('div');
+            itemDiv.hide();
+        })
     }
 
-    images.hover(event => {
-        const target = $(event.target);
-        const itemDiv = target.next('div');
-        const x = event.clientX;
-        const y = event.clientY;
-        const itemDivWidth = itemDiv.outerWidth();
-        const itemDivHalfHeight = itemDiv.height() / 2;
-        itemDiv.css({ top: (y - itemDivHalfHeight) + 'px', left: (x - itemDivWidth) + 'px' });
-        itemDiv.show();
-    }, event => {
-        const target = $(event.target);
-        const itemDiv = target.next('div');
-        itemDiv.hide();
-    })
-}
-
-showItems();
+    showItems();
+}())
